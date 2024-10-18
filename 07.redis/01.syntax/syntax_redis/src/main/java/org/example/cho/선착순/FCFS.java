40f8d191da603dc3d5cd1592e3be206c0c7ea829
Doohwancho <doohwancho1993@gmail.com>
2024-10-18 16:55:35 +0900

    E-e-2. 선착순 100명에 대기큐에 추가 설명

--- 07.redis/01.syntax/syntax_redis/src/main/java/org/example/cho/선착순/FCFS.java
+++ 07.redis/01.syntax/syntax_redis/src/main/java/org/example/cho/선착순/FCFS.java
@@ -2,6 +2,17 @@ package org.example.cho.선착순;
 
 
 /*
+    Q. 왜 java lock(e.g synchronized)나 mysql/redis lock으로 구현 안함?
+    A. 1. java lock은 scale out 환경에서는 못쓴다.
+       2. 수만명이 한 곳에 접근해서 lock걸고 read~write 하는 구조는 lock contention 때문에 자원소모도 크고 latency도 길어진다.
+       3. queue에 넣고 순서대로 빼는 것, 그리고 일정 수준 이상 이면 안넣는게 lock contention을 피하면서 선착순 시스템 구현하기 적합한 구조이다.
+    
+    Q. 어떻게 이 코드를 활용?
+    A. step1) coupon repository extend하던가 redis만을 위한 repository 만들어서,
+       step2) queue에 선착순 100명 유저 저장한 이후,
+       step3) 본 작업(e.g 선착순 100명에게 쿠폰 발급한다던가..)을 @Transaction 내에 여러 타 작업과 같이 하면 됨
+    
+    
     Q. 관련 redis command?
     A. 1. 사용자 등록: LPUSH event:queue user_id
        2. 큐 길이 제한(100명): LTRIM event:queue 0 99
